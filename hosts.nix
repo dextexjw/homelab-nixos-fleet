2685e264a704df02f165251f5594c7cc9cfecd66
dexterjw <dev@jax22.com>
2026-05-27 10:56:53 -0400

    feat(virtual-machines): adjust resource allocation for gateway-vm and add productivity-vm and security-vm configurations

--- hosts.nix
+++ hosts.nix
@@ -20,11 +20,11 @@
     ];
     timezone = "America/New_York";
     vm = {
-      cores = 4;
+      cores = 2;
       disk = "/dev/sda";
       id = "112";
       name = "gateway-vm";
-      ramGB = 8;
+      ramGB = 3;
     };
   };
 
@@ -49,6 +49,54 @@
       id = "113";
       name = "media-vm";
       ramGB = 8;
+    };
+  };
+
+  productivity-vm = {
+    arch = "x86_64-linux";
+    domain = "home.arpa";
+    fqdn = "productivity.home.arpa";
+    ip = "10.2.20.114";
+    gateway = "10.2.20.1";
+    nameservers = [
+      "10.2.20.1"
+      "9.9.9.9"
+    ];
+    user = "smoke";
+    tags = [
+      "productivity"
+    ];
+    timezone = "America/New_York";
+    vm = {
+      cores = 4;
+      disk = "/dev/sda";
+      id = "114";
+      name = "productivity-vm";
+      ramGB = 8;
+    };
+  };
+
+  security-vm = {
+    arch = "x86_64-linux";
+    domain = "home.arpa";
+    fqdn = "security.home.arpa";
+    ip = "10.2.20.115";
+    gateway = "10.2.20.1";
+    nameservers = [
+      "10.2.20.1"
+      "9.9.9.9"
+    ];
+    user = "smoke";
+    tags = [
+      "security"
+    ];
+    timezone = "America/New_York";
+    vm = {
+      cores = 2;
+      disk = "/dev/sda";
+      id = "115";
+      name = "security-vm";
+      ramGB = 4;
     };
   };
 }
